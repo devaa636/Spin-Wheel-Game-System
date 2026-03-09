43558475014b2f6d33510604496497dbf354dfce
Devansh Agarwal <devansh2005agrawal@gmail.com>
2026-03-10 02:09:45 +0530

    Spin wheel creation handler added

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const server = http.createServer((req,res)=>{
 
  const parsedUrl = url.parse(req.url,true)
 
- if(parsedUrl.pathname === "/"){
-  res.end("Spin Wheel Server Running")
+ if(parsedUrl.pathname === "/createWheel"){
+  createWheel(res)
  }
 
 })
@@ -14,3 +14,26 @@ const server = http.createServer((req,res)=>{
 server.listen(3000,()=>{
  console.log("Server running on port 3000")
 })
+
+const db = require("./db")
+
+function createWheel(res){
+
+ db.query(
+ "SELECT * FROM spin_wheels WHERE status='waiting'",
+ (err,result)=>{
+
+ if(result.length>0){
+   res.end("Wheel already active")
+   return
+ }
+
+ db.query(
+ "INSERT INTO spin_wheels(entry_fee,status) VALUES (100,'waiting')"
+ )
+
+ res.end("Wheel created")
+
+ })
+
+}
